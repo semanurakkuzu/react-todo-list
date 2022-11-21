48f54300aca7e588e29f77d3f0bf511ae242e550
semanurakkuzu <semanurakkuzu@gmail.com>
2022-11-21 17:56:17 +0300

    added delete and checkdone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [todoInput, setTodoInput] = useState("");
 
+  useEffect(() => {
+    document.title = `Todo Item Count: ${todos.length}`;
+  });
+
   function todoInputChange(event) {
-    setTodoInput(event.target.value)
+    setTodoInput(event.target.value);
   }
 
   function addNewTodo() {
-    setTodos([todoInput, ...todos])
+    setTodos([{text:todoInput, isDone:false}, ...todos]);
+  }
+
+  function deleteTodo(index) {
+    setTodos(todos.filter((_, todoIndex) => todoIndex !== index));
+  }
+
+  function checkDone(index) {
+    setTodos(todos.map((todo, todoIndex) => {
+      if (todoIndex !== index) {
+        return todo;
+      }
+
+      todo.isDone = !todo.isDone;
+      return todo;
+    }))
   }
 
   return (
     <>
       <div className="container">
-      <p className="fs-1 text-center my-5">To Do List</p>
+        <p className="fs-1 text-center my-5">To Do List</p>
         <header className="row mb-3 justify-content-center">
-        <div className="col-6">
+          <div className="col-6">
             <input
               type="text"
               className="form-control"
@@ -37,26 +56,29 @@ function App() {
           </div>
         </header>
         {todos.map((todo, index) => (
-          <section className="row mb-3 justify-content-center" id={index}>
+          <section className="row mb-3 justify-content-center" key={index}>
             <div className="col-6">
               <ul className="list-group">
                 <li className="list-group-item">
-                  <input
-                    className="form-check-input me-1"
-                    type="checkbox"
-                    id="firstCheckboxStretched"
-                  />
                   <label
                     className="form-check-label stretched-link"
-                    for="firstCheckboxStretched"
+                    htmlFor="firstCheckboxStretched"
+                    style={{cursor: 'pointer'}} onClick={() => checkDone(index)}
                   >
-                    {todo}
+                    {todo.isDone
+                     ? <s>{todo.text}</s>
+                     : <span>{todo.text}</span>
+                    }
                   </label>
                 </li>
               </ul>
             </div>
             <div className="col-1">
-              <button className="btn btn-outline-secondary" type="button">
+              <button
+                className="btn btn-outline-secondary"
+                type="button"
+                onClick={() => deleteTodo(index)}
+              >
                 Delete
               </button>
             </div>
